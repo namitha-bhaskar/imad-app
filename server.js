4fa0c6cf849f2c6f2541f19ab7d87f011a1c6d43
namitha-bhaskar <30932950+namitha-bhaskar@users.noreply.github.com>
2017-08-16 20:57:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one',function(req,res){
+    res.send('arrticle one requested here');
+});
+app.get('/article-two',function(req,res){
+    res.send('article two requested here');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
